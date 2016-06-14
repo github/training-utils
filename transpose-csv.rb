5f4b3fd53c54dbe0e5147c0a07a38fa5fdb94dff
Steve Winton <stevewinton@gmail.com>
2016-06-13 20:19:21 -0500

    Specify file encoding when transcoding CSVs

--- transpose-csv.rb
+++ transpose-csv.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 require 'CSV'
 
+file = File.open(ARGV[0], "r:ISO-8859-1:ISO-8859-1")
+rows = CSV.parse(file)
+
 # Via https://stackoverflow.com/questions/29521170/ruby-transpose-csv
-rows = CSV.new($stdin).read
 puts rows.transpose.map { |x| x.join ',' }
