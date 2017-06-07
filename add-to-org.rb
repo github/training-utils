971764d9f70293d3ab5a6f61d214eb99021c663e
Brent Beer <brent.beer@gmail.com>
2017-06-07 13:29:25 +0200

    appropriate key names in the options hash

--- add-to-org.rb
+++ add-to-org.rb
@@ -16,15 +16,15 @@ OptionParser.new do |opts|
   opts.banner = "Usage: add-to-org.rb [options]"
 
   opts.on("-u", "--username USERNAME", "Username for membership -- ex: githubstudent") do |i|
-    options[:issue] = u
+    options[:username] = u
   end
 
   opts.on("-o", "--org ORGANIZATION", "Organization name -- ex: githubschool") do |r|
-    options[:repo] = o
+    options[:org] = o
   end
 
   opts.on("-t", "--team TEAM_NAME", "Team to add member to -- ex: developers") do |i|
-    options[:issue] = t
+    options[:team] = t
   end
 
   opts.on_tail("-h", "--help", "Prints this help message") do |h|
