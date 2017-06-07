76cb05e956c2dbbb0eb458eb2e92756f9d035f09
Brent Beer <brent.beer@gmail.com>
2017-06-07 13:18:30 +0200

    shell of add-to-org script, copied from add-collaborators

---
+++ add-to-org.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+# add USERNAME as member to ORGANIZATION and optionally to TEAM
+# assumes you created and stored the appropriate OAuth token as an ENV variable called GITHUB_TOKEN
+
+require 'octokit'
+require 'optparse'
+
+# Make sure arguments are specified
+ARGV << '-h' if ARGV.empty?
+
+# Create options hash
+options = {}
+
+# Parse options
+OptionParser.new do |opts|
+  opts.banner = "Usage: add-collaborators.rb [options]"
+
+  opts.on("-u", "--username USERNAME", "Username for membership -- ex: githubstudent") do |i|
+    options[:issue] = u
+  end
+
+  opts.on("-o", "--org ORGANIZATION", "Organization name -- ex: githubschool") do |r|
+    options[:repo] = o
+  end
+
+  opts.on("-t", "--team TEAM_NAME", "Team to add member to -- ex: developers") do |i|
+    options[:issue] = t
+  end
+
+  opts.on_tail("-h", "--help", "Prints this help message") do |h|
+    puts opts
+    exit
+  end
+
+end.parse!
+
+puts options
